9ba887beace4857d2c3fb6f4dd5916be06857557
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-03-07 17:50:26 +0100

    Update State.java

--- lab 5/src/Simulator/State.java
+++ lab 5/src/Simulator/State.java
@@ -3,19 +3,64 @@ import java.util.Observable;
 
 import State.StoreState;
 import Time.*;
+import Event.*;
 
 public class State extends Observable {
+	
+	private Store store;
 	private Boolean StopFlag;
-	private Double Time;
-	private Double currentTime;
+	private double Time;
+	private double currentTime;
+	
+	private double currentTime;
+	private double lastEventTime;
+	
+	private UniformRandomStream payTime;
+	private UniformRandomStream pickTime;
+	private ExponentialRandomStream arrivalTime;
+	
+	private Event currentEvent;
+	private Customer currentCustomer;
+	
+//	private long seed;
+//	
+//	private double lambda;
+//	private double payMinTime;
+//	private double payMaxTime;
+//	private double pickMinTime;
+//	private double pickMaxTime;
 	
 
 	public State() {
-	StoreState store = new StoreState();
-	
-		
+		StoreState store = new StoreState();
+		currentTime = 0;
+		lastEventTime = 0;
 	}
-
 	
+	public Store getStore() {
+		return store;
+	}
+	
+	public void update(Event thisEvent) {
+		currentEvent = thisEvent;
+		
+		
+		lastEventTime = currentEventTime;
+		currentTime = thisEvent.getTime();
+
+		
+		if(thisEvent.getClass() != Stop.class) {
+			
+			
+			if(thisEvent.getClass() == Pay.class) {
+				store.setLastPaymentTime(thisEvent.getTime());
+			}
+		} else {
+			
+		}
+		
+		setChanged();
+		notifyObservers();
+	}
 
 }
