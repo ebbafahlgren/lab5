10ce1d6b61e42d892f04c957a99e0944fb27df21
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-03-08 12:53:51 +0100

    Update Pay.java

--- lab 5/src/Event/Pay.java
+++ lab 5/src/Event/Pay.java
@@ -1,11 +1,6 @@
-//Jag är världssämst så denna är inte korrekt men tror inte själva uppbyggnaden är så fel. När jag programmerade i min Eclipse ändrade jag några metoder
-//till public i andra klasser men vågade inte röra något i vårt gemensamma
-
 package Event;
-import java.awt.EventQueue;
-import java.lang.Thread.State;
 
-import Simulator.Event;
+import Simulator.*;
 import State.Customer;
 import State.FIFO;
 
@@ -13,37 +8,37 @@ public class Pay extends Event {
 
 	private double time;
 	private Customer customer;
+	private State state;
+	private EventQueue eventQueue;
+	private int counterPayments = 0;
 
 	public Pay(State state, EventQueue eventQueue, double time, Customer customer) {
-		super(state, eventqueue); //försöker ärva in från Event
+		super(state, eventQueue); //försöker ärva in från Event
 	     
 		//skapar nya objekt 
 		this.time = time; 
 	    this.customer = customer; 
-	      
 		
 	}
 
 	@Override
 	public void doThis() {
 		
-		int counterPayments = 0;
-		int customerInStore = arrival //tänker att det kommer finnas en räknare i arrival som räknar antal inkommande?
+		//int customerInStore = arrival; //tänker att det kommer finnas en räknare i arrival som räknar antal inkommande?
 		
-		 state.update(this);
+		state.update(this);
 	      
-	      state.getStore().addMoney(); //registrera att pengar lagts till i getStore i State
-	      state.getStore().removeCustomer(); //registera att kund ska tas bort
-	      state.getStore().freeCheckOut(); //registera att kassan blir ledig
+	    state.getStore().customerPayed(); //registrera att kunder betalat
+	    state.getStore().removeCustomer(); //registera att kund ska tas bort
+	    state.getStore().freeCheckOut(); //registera att kassan blir ledig
 	      
-	      if (FIFO.queue != null); //om FIFOkön inte är tom
-	      FIFO.removeFirst(); //ropas removeFirst och personen först i kön tas bort
-	      
-	      Pay payEvent = new Pay(this.state, this.eventqueue); //skapar själva betalningen
-	        counterPayments ++; //räknar betalningar
-		customerInStore --; //uppdaterar antal kunder i butiken
-	        
-		
+	    if (state.getStore().getRegisterQueue() != null) { //om FIFOkön inte är tom
+	    	state.getStore().getRegisterQueue().removeFirst(); //ropas removeFirst och personen först i kön tas bort
+	    }
+	    
+	    Pay payEvent = new Pay(state, eventQueue, time, customer); //skapar själva betalningen
+	    counterPayments++; //räknar betalningar
+		customerInStore--; //uppdaterar antal kunder i butiken
 	}
 
 	@Override
@@ -53,9 +48,14 @@ public class Pay extends Event {
 	}
 
 	@Override
-	public Customer getCustoumer() {
+	public Customer getCustomer() {
 		// TODO Auto-generated method stub
-		return null;
+		return customer;
+	}
+	
+	@Override
+	public String writeOut() {
+		return "Paying...";
 	}
 
 }
