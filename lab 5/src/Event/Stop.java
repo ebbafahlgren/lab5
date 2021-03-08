d4ce03326873ccfe4074563621554dfe1d05d094
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-03-08 12:55:17 +0100

    Update Stop.java

--- lab 5/src/Event/Stop.java
+++ lab 5/src/Event/Stop.java
@@ -1,11 +1,38 @@
 package Event;
+
 import Simulator.Event;
-public class Stop extends Event{
+import Simulator.EventQueue;
+import Simulator.State;
+import State.Customer;
+import State.StoreState;
 
-	public Stop() {
-		// TODO Auto-generated constructor stub
+public class Stop extends Event {
+	private double time;
+	private State state;
+	private EventQueue eventQueue;
+
+	public Stop(State state, EventQueue eventQueue) {
+		super(state, eventQueue);
+		this.time = 999;
 	}
-	
-	
 
+	public double getTime() {
+		return time;
+	}
+
+	@Override
+	public void doThis() {
+		state.update(this);
+		state.turnOfSimulator(); // Den ska sluta köra simulator
+	}
+
+	@Override
+	public Customer getCustomer() {
+		return null;
+	}
+
+	@Override
+	public String writeOut() {
+		return "Stop";
+	}
 }
