6555509024d10d77588e63bb4e76b1b786b440d6
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-03-08 12:57:11 +0100

    Update Customer.java

--- lab 5/src/State/Customer.java
+++ lab 5/src/State/Customer.java
@@ -1,24 +1,31 @@
 package State;
 
+import State.CreateCustomer.customerStatus;
+
 public class Customer {
 	
 	private int customerID;
-//	private customer state;
+	private customerStatus state;
 
+	//Vet inte hur kundens id ska tilldelas
 	public Customer(int customerInt) {
 		this.customerID = customerInt;
 	}
 	
-	public int getCustomerID() {
+	public int getCustoumerID() {
 		return customerID;
 	}
 	
-	public CustomerState getState() {
+	public customerStatus getState() {
 		return state;
 	}
 	
-	public void setState(CustomerState state) {
+	public void setState(customerStatus state) {
 		this.state = state;
 	}
 
+	public String getID() {
+		return String.valueOf(customerID);
+	}
+	
 }
