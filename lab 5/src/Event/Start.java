7c9b7e3377da4ba972d1d2dc9cb68d7f7e8550ab
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-03-08 12:55:00 +0100

    Update Start.java

--- lab 5/src/Event/Start.java
+++ lab 5/src/Event/Start.java
@@ -1,9 +1,62 @@
 package Event;
-import Simulator.Event;
-public class Start extends Event{
 
-	public Start() {
-		// TODO Auto-generated constructor stub
+import Simulator.Event;
+import Simulator.EventQueue;
+import Simulator.State;
+import State.StoreState;
+
+import State.*;
+
+public abstract class Start extends Event {
+	private Close closing;
+	private Arrival arrival;
+	private State state;
+	private EventQueue eventQueue;
+	private Close closeStore;
+
+	private double closeTime;
+	private double time;
+
+	public Start(State state, EventQueue eventQueue) {
+		super(state, eventQueue);
+		this.time = 0d; ///????
+		this.closeTime = state.getClosingTime();
 	}
 
+	@Override
+	public void doThis() {
+      state.update(this);
+      
+      state.getStore().setStoreOpen(true);
+      
+      closeStore = new Close(this.state, eventQueue, closeTime);
+      
+      eventQueue.SortedSequence(closing);
+      
+      double arrivalTime = 0;
+      
+      //inte ligga här i start.. Start initieras bara när simulatorn kör igång.
+      while(closeTime >  arrivalTime) {
+    	  arrivalTime += state.arrivalTime();
+          arrival = new Arrival(this.state, this.eventQueue, arrivalTime);
+          eventQueue.SortedSequence(arrival);
+          break;
+      }
+    }
+      
+	@Override
+	public double getTime() {
+		return time;
+	}
+
+	@Override
+	public Customer getCustomer() {
+		return null;
+	}
+
+	@Override
+	public String writeOut() {
+		return "Start";
+	}
 }
+
