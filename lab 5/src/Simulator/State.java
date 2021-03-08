3aed08556c06f876eab339ff38b15f78fb78a2df
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-03-08 12:56:32 +0100

    Update State.java

--- lab 5/src/Simulator/State.java
+++ lab 5/src/Simulator/State.java
@@ -1,18 +1,21 @@
 package Simulator;
 import java.util.Observable;
 
+import State.*;
 import State.StoreState;
 import Time.*;
-import Event.*;
+import Time.Time;
+import Event.*; 
 
 public class State extends Observable {
 	
-	private Store store;
+	private StoreState store;
 	private Boolean StopFlag;
-	private double Time;
+	private double Time; //tid butiken ska vara öppen?
 	private double currentTime;
+	private double currentEventTime;
+	//private Time time = new Time();
 	
-	private double currentTime;
 	private double lastEventTime;
 	
 	private UniformRandomStream payTime;
@@ -22,22 +25,55 @@ public class State extends Observable {
 	private Event currentEvent;
 	private Customer currentCustomer;
 	
-//	private long seed;
-//	
-//	private double lambda;
-//	private double payMinTime;
-//	private double payMaxTime;
-//	private double pickMinTime;
-//	private double pickMaxTime;
+	private long seed;
 	
-
-	public State() {
-		StoreState store = new StoreState();
-		currentTime = 0;
-		lastEventTime = 0;
+	private double lambda;
+	private double payMinTime;
+	private double payMaxTime;
+	private double pickMinTime;
+	private double pickMaxTime;
+	
+	private int maxCustomers;
+	private int numRegisters;
+	private double closingTime;	
+		
+	
+	private ExponentialRandomStream customerArrived;
+	private UniformRandomStream customerPick, customerPay; 
+	
+	public void Time(double lambda, long seed, double minPick, double maxPick, double minPay, double maxPay) {
+		this.customerArrived = new ExponentialRandomStream(lambda, seed);
+		this.customerPay = new UniformRandomStream(minPick, maxPick, seed);
+		this.customerPick = new UniformRandomStream(minPay, maxPay, seed);
 	}
 	
-	public Store getStore() {
+	// returnerar nästa tid för en arrivalhändelse (Exponential)
+	public double arrivalTime() {
+		return customerArrived.next();
+	}
+	
+	// returnerar nästa tid för en pickhändelse (Uniform)
+	public double timePick() {
+		return customerPick.next();
+	}
+	
+	// returnerar nästa tid för en payhändelse (Uniform)
+	public double timePay() {
+		return customerPay.next();
+	}
+
+	public State() {
+		StoreState store = new StoreState(numRegisters, closingTime, maxCustomers);
+		currentTime = 0;
+		lastEventTime = 0;
+		closingTime = closingTime;
+	}
+	
+	public double getClosingTime() {	
+		return closingTime;
+	}
+
+	public StoreState getStore() {
 		return store;
 	}
 	
@@ -59,8 +95,22 @@ public class State extends Observable {
 			
 		}
 		
+		if (stopFlag == true) {
+			turnOfSimulator();
+		}
+		
+		if (time > closingTime) {
+			store.
+		}
+		
 		setChanged();
 		notifyObservers();
 	}
+
+	public void turnOfSimulator() {
+		// TODO Auto-generated method stub
+		simulator.stop
+	}
+
 
 }
