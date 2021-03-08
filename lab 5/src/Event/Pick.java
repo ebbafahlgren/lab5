93fb87e6199523fbd0a0ba9eb24074697a122581
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-03-08 12:54:27 +0100

    Update Pick.java

--- lab 5/src/Event/Pick.java
+++ lab 5/src/Event/Pick.java
@@ -1,9 +1,41 @@
 package Event;
 import Simulator.Event;
+import Simulator.EventQueue;
+import Simulator.State;
+import State.Customer;
 public class Pick extends Event{
 
-	public Pick() {
-		// TODO Auto-generated constructor stub
+	private Customer customer;
+	private double time;
+	private State state;
+	
+	private Pay payEvent;
+	
+	public Pick(State state, EventQueue eventQueue, Customer customer, double time) {
+		super(state, eventQueue);
+		
+		this.customer = customer;
+		this.time = time;
 	}
 
+	@Override
+	public void doThis() {
+		state.update(this);
+	}
+	
+	@Override
+	public double getTime() {
+		return time;
+	}
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+	
+	@Override
+	public String writeOut() {
+		return "Pickevent";
+	}
+	
 }
+
