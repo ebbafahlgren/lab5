537d58b55cde7762e64550d85c8f58d2d78fd2f3
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-03-10 17:40:12 +0100

    Add files via upload

---
+++ lab5snartklarkanske/src/Event/Arrival.java
@@ -0,0 +1,91 @@
+package Event;
+
+import Simulator.*;
+
+import Simulator.*;
+import State.CreateCustomer.customerStatus;
+import State.Customer;
+import State.StoreState;
+import Simulator.State;
+import Time.*;
+
+/**
+ * @author Ebba Fahlgren, Anton Sandberg, Emma Evergren och Erik Hilmersson
+ *
+ */
+
+public class Arrival extends Event {
+	private Pick pickEvent;
+	private double time;
+	private Customer customer;
+	private State state;
+	private EventQueue eventQueue;
+	//private pickTime picktime = new pickTime();
+
+	//private Pick pickevent = new Pick(state, eventQueue, customer, time);
+	
+	public Arrival(State state, EventQueue eventQueue, double time) {
+		super(state, eventQueue);
+		this.time = time;
+		//System.out.println(this.time);
+		
+		this.state = state;
+		customer = this.state.getStore().createCustomer();
+		//System.out.println("arrival!");
+		//System.out.println(eventQueue + " eventkön direkt i arrival");
+//		System.out.print(customer.getState());
+//		customer.setState(customerStatus.inStore);
+//		
+//		System.out.print(customer.getState());
+		//this.eventQueue = eventQueue;
+		this.eventQueue = eventQueue;
+
+	}
+
+	@Override
+	public void doThis() {
+		state.update(this);
+		
+		this.state.getStore().addCustomer(customer);
+		
+		//System.out.print(customer.getState());
+		if (customer.getState() == customerStatus.inStore) {
+			
+			//double pickTime = this.time + state.timePick(); //
+			
+			double a = state.timePick();
+			double pickTime = this.time + a;
+			//System.out.println(a);
+			//System.out.println(pickTime + " uträknad");
+			//System.out.println(this.eventQueue + " eventkö");
+			//System.out.println(this.time + " this time inuti do this");
+			//System.out.println(state.timePick() + " plocktid");
+			
+			//System.out.println(pickTime + " picktime");
+			
+			pickEvent = new Pick(state, eventQueue, customer, pickTime);
+			//System.out.println(pickEvent);
+			
+			//System.out.println(eventQueue + " = vad som finns i eventQueue");
+			eventQueue.SortedSequence(pickEvent);
+			
+			
+		}
+	}
+
+	@Override
+	public double getTime() {
+		return time;
+	}
+
+	@Override
+	public Customer getCustomer() {
+		return customer;
+	}
+	
+	@Override
+	public String writeOut() {
+		return "Arrival";
+	}
+
+}
---
+++ lab5snartklarkanske/src/Event/Start.java
@@ -0,0 +1,86 @@
+package Event;
+
+import Simulator.Event;
+import Simulator.EventQueue;
+import Simulator.State;
+import State.StoreState;
+
+import State.*;
+
+/**
+ * @author Ebba Fahlgren, Anton Sandberg, Emma Evergren och Erik Hilmersson
+ *
+ */
+
+public class Start extends Event {
+	private Close closing;
+	private Arrival arrival;
+	private State state;
+	private EventQueue eventQueue;
+	private Close closeStore;
+	private StoreState store;
+
+	private double closeTime;
+	private double time;
+
+	public Start(State state, EventQueue eventQueue) {
+		
+		super(state, eventQueue);
+		
+		System.out.println(state.getClosingTime() + " tid för stängning");
+		
+		this.time = 0d; /// ????
+		this.closeTime = state.getClosingTime(); //state.getStore().getClo... funkar ej!
+		this.state = state; //kanske ha med denna
+		this.eventQueue = eventQueue;
+	}
+
+	@Override
+	public void doThis() {
+		//System.out.println("Do this i start");
+		
+		//state.update(this);
+
+		state.getStore().setStoreOpen(true);
+		//state.getStore();
+		//System.out.println(state.getStore() + " butiken");
+		//store.setStoreOpen(true);
+		//storeTest.get
+		
+		closeStore = new Close(this.state, eventQueue, closeTime);
+
+		//System.out.println("Sortera in closing");
+		eventQueue.SortedSequence(closeStore);
+
+		double arrivalTime = 0;
+		
+		while(closeTime > arrivalTime) {
+			arrivalTime = arrivalTime + state.arrivalTime();
+			arrival = new Arrival(this.state, this.eventQueue, arrivalTime);
+			eventQueue.SortedSequence(arrival);
+		}
+
+		// inte ligga här i start.. Start initieras bara när simulatorn kör igång.
+//      while(closeTime >  arrivalTime) {
+//    	  arrivalTime += state.arrivalTime();
+//          arrival = new Arrival(this.state, this.eventQueue, arrivalTime);
+//          eventQueue.SortedSequence(arrival);
+//          break;
+//      }
+	}
+
+	@Override
+	public double getTime() {
+		return time;
+	}
+
+	@Override
+	public Customer getCustomer() {
+		return null;
+	}
+
+	@Override
+	public String writeOut() {
+		return "Start";
+	}
+}
