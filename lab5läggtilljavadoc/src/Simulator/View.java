e001136e412136eb356c2d0f0ddf000ea4787f6a
emmaevergren <79854381+emmaevergren@users.noreply.github.com>
2021-03-11 12:28:09 +0100

    Update View.java

--- lab5läggtilljavadoc/src/Simulator/View.java
+++ lab5läggtilljavadoc/src/Simulator/View.java
@@ -7,7 +7,6 @@ import State.*;
 
 /**
  * @author Ebba Fahlgren, Anton Sandberg, Emma Evergren och Erik Hilmersson
- *
  */
 
 public abstract class View implements Observer {
@@ -15,10 +14,18 @@ public abstract class View implements Observer {
    State state;
    StoreState store;
    
+    /**
+    * @param state statusen
+    * @param store butiken
+    */
    public View(State state, StoreState store)
    {
       state.addObserver(this);
    }
+   
+   /**
+    * @param arg arg
+    */
     @Override
     abstract public void update(Observable o, Object arg);
 
