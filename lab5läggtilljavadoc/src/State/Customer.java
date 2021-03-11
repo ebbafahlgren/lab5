a2af9416e03ec2b4fde5fbab3e34427a94180732
emmaevergren <79854381+emmaevergren@users.noreply.github.com>
2021-03-11 12:32:38 +0100

    Update Customer.java

--- lab5läggtilljavadoc/src/State/Customer.java
+++ lab5läggtilljavadoc/src/State/Customer.java
@@ -11,23 +11,38 @@ public class Customer {
 	
 	private int customerID;
 	private customerStatus state;
-
+	
+	/**
+	 * @param customerInt ger en siffra till customerID
+	 */
 	public Customer(int customerInt) {
 		this.customerID = customerInt;
 	}
 	
+	/**
+	 * @return customerID
+	 */
 	public int getCustomerID() {
 		return customerID;
 	}
 	
+	/**
+	 * @return state
+	 */
 	public customerStatus getState() {
 		return state;
 	}
 	
+	/**
+	 * @param state status uppdateras till det aktuelle
+	 */
 	public void setState(customerStatus state) {
 		this.state = state;
 	}
 
+	/**
+	 * @return strängen av customerID
+	 */
 	public String getID() {
 		return String.valueOf(customerID);
 	}
