c70983514edc153faab0d1b5a32d455248631cb4
emmaevergren <79854381+emmaevergren@users.noreply.github.com>
2021-03-20 16:22:21 +0100

    Create module-info.java

---
+++ lab5nyaFUNKAR/src/State/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Statepaketet beskriver de specifika tillstånden och innehåller en FIFO klass för kassakön
+ * en CreateCustomer som skapar nya kunder och en Customerklass som returnerar specifik customer.
+ * StoreState beskriver allt som sker i butiken och StoreTime räknar ut tiden och sparar den.
+ *
+ */
+
+package State;
