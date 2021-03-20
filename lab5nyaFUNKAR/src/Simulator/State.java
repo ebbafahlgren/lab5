5025cf8b8d454f6db696f43a31ccbf52e084ca1a
emmaevergren <79854381+emmaevergren@users.noreply.github.com>
2021-03-20 16:18:45 +0100

    Update State.java

--- lab5nyaFUNKAR/src/Simulator/State.java
+++ lab5nyaFUNKAR/src/Simulator/State.java
@@ -10,19 +10,37 @@ import Event.*;
  * @author Ebba Fahlgren, Anton Sandberg, Emma Evergren och Erik Hilmersson
  */
 
+/**
+ * state is a class that determines if the simulation is running or not. We also observe the state so if anything is to be changed the update function is called.
+ *
+ */
+
 public class State extends Observable {
 
 	private boolean stopFlag = false;
 
 
+/**
+ * turnOfSimulation turns of the simulation and set the stopflag to be true.
+ *
+ */
 	public void turnOfSimulation() {
 		stopFlag = true;
 		update();
 	}
+	
+/**
+ * @return stopFlag
+ *
+ */
 	public boolean getStopFlag() {
 
 		return stopFlag;
 	}
+/**
+ * update let the observer know that something has changed
+ *
+ */
 	public void update() {
 		setChanged();
 		notifyObservers();
